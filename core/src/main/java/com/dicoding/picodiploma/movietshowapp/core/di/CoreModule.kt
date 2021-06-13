d35863e27c011fa7b377486683c62dd9411ea4e1
Evandio <evandiomb@gmail.com>
2021-06-13 17:39:04 +0700

    added security and leak canary

--- core/src/main/java/com/dicoding/picodiploma/movietshowapp/core/di/CoreModule.kt
+++ core/src/main/java/com/dicoding/picodiploma/movietshowapp/core/di/CoreModule.kt
@@ -8,6 +8,9 @@ import com.dicoding.picodiploma.movietshowapp.core.data.source.remote.RemoteData
 import com.dicoding.picodiploma.movietshowapp.core.data.source.remote.network.ApiService
 import com.dicoding.picodiploma.movietshowapp.core.domain.repository.IMovieRepository
 import com.dicoding.picodiploma.movietshowapp.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,19 +22,30 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<MovieDatabase>().movieDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             MovieDatabase::class.java, "Movie.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "tourism-api.dicoding.dev"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/uR1H6l4jM+/HBDx6jZNticZim+NHBsQYOpO5t/6pJa4=")
+            .add(hostname, "sha256/cXjPgKdVe6iojP8s0YQJ3rtmDFHTnYZxcYvmYGFiYME=")
+            .add(hostname, "sha256/hxqRlPTu1bMS/0DITB1SSu0vd4u/8l8TjPgfaAp63Gc=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
